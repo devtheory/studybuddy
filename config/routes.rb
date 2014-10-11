873c1096cdf0a06697f9cbb691060cdbe2905f96
Jose Sanchez <jparkers0012@gmail.com>
2014-10-11 14:22:39 -0400

    authentication added with facebook auth and local

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+  get 'sign_out', :to => 'devise/sessions#destroy'#, :as => :destroy_user_session
+  end
 
   get 'about' => 'welcome#about'
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011032108) do
+ActiveRecord::Schema.define(version: 20141011150029) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20141011032108) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
